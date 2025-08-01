10ccb0210391fa9a891180d013b5647add1db42a
배석현 <bjh4966@naver.com>
2025-08-02 00:56:45 +0900

    [FEAT] Category 추가

--- src/main/java/server/loop/domain/post/entity/Category.java
+++ src/main/java/server/loop/domain/post/entity/Category.java
@@ -3,5 +3,11 @@ package server.loop.domain.post.entity;
 public enum Category {
     FREE,  // 자유게시판
     USED,   // 중고게시판 (나중에 추가할 것)
-    MEET
+    MEET,
+    BATCH_2020,  // 2020 Batch Board
+    BATCH_2021,  // 2021 Batch Board
+    BATCH_2022,  // 2022 Batch Board
+    BATCH_2023,  // 2023 Batch Board
+    BATCH_2024,  // 2024 Batch Board
+    BATCH_2025   // 2025 Batch Board
 }
