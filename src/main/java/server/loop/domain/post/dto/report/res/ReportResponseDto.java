9b0c07dd5d25b299bf325391a01293b8fbebbe94
배석현 <bjh4966@naver.com>
2025-08-11 17:19:33 +0900

    [FIX] report controller json 반환

---
+++ src/main/java/server/loop/domain/post/dto/report/res/ReportResponseDto.java
@@ -0,0 +1,14 @@
+package server.loop.domain.post.dto.report.res;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReportResponseDto {
+    private String message;
+}
